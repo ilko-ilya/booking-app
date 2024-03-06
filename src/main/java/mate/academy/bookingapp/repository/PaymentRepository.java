762fd5076c617aab04c092173432512c3885d3e8
ilko-ilya <samilyakilya@gmail.com>
2024-03-06 22:48:38 +0200

    added paymentController tests and fixed some mistakes

--- src/main/java/mate/academy/bookingapp/repository/PaymentRepository.java
+++ src/main/java/mate/academy/bookingapp/repository/PaymentRepository.java
@@ -3,6 +3,7 @@ package mate.academy.bookingapp.repository;
 import java.util.List;
 import java.util.Optional;
 import mate.academy.bookingapp.model.Payment;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,7 +23,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     @Query("SELECT p FROM Payment p "
             + "JOIN Booking b ON p.bookingId = b.id "
             + "WHERE b.user.id = :userId")
-    List<Payment> findPaymentsByUserId(@Param("userId") Long userId);
+    Page<Payment> findPaymentsByUserId(@Param("userId") Long userId, Pageable pageable);
 
     @Query("SELECT p FROM Payment p "
             + "JOIN Booking b ON p.bookingId = b.id "
